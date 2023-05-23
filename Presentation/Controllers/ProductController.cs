8f2f522883f46045fe224aff3c2267a452eef452
muhammedbayindir <OttomanState3@hotmail.com>
2023-05-24 00:11:37 +0300

    AssemblyReferance

---
+++ Presentation/Controllers/ProductController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Entities.Models;
+using Microsoft.AspNetCore.Mvc;
+using Services.Contract;
+
+namespace Presentation.Controllers
+{
+	[ApiController]
+	[Route("api/product")]
+	public class ProductController:ControllerBase
+	{
+		private readonly IServiceManager _serviceManager;
+
+        public ProductController(IServiceManager serviceManager)
+        {
+            _serviceManager = serviceManager;
+        }
+
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+
+            return Ok(_serviceManager.productService.GetProducts(false));
+        }
+        [HttpGet("{id}")]
+        public IActionResult GetProduct([FromRoute(Name = "id")] int id)
+        {
+            return Ok(_serviceManager.productService.GetOneProductById(id, false));
+        }
+        [HttpPost]
+        public IActionResult AddProduct([FromBody] Product product)
+        {
+            _serviceManager.productService.CreateOneProduct(product);
+            return StatusCode(201, product);
+        }
+    }
+}
+
--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -6,7 +6,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services
+    .AddControllers()
+    .AddApplicationPart(typeof(Presentation.AssemblyReferance).Assembly);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.ConfigurSqlContext(builder.Configuration);
